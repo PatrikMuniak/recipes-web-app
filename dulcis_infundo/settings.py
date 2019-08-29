debc662275c7c64bb033e8ddd664908ca44bd0f5
PatrikMuniak <50305097+PatrikMuniak@users.noreply.github.com>
2019-08-29 10:44:42 +0100

    Update settings.py

--- dulcis_infundo/settings.py
+++ dulcis_infundo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#)k3@)vp$@@_%q%_^-7z&yyvw*799e_zga_z&)szmro#r1k^4!'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
